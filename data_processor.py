784c296178dd2035ad51825c747080e604571e5d
Atsu Vovor <atsu.vovor@gmail.com>
2025-11-08 23:14:57 -0500

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -4,7 +4,7 @@ import os
 from typing import Optional, Dict, Any, Tuple
 import pandas as pd
 import numpy as np
-from config.config import LLM_MODEL_PATH, USE_RAG, RISK_COLOR_MAP
+from config import LLM_MODEL_PATH, USE_RAG, RISK_COLOR_MAP
 
 
 # Constants
