883e395e9335f0333ff3629ca5633b86794b5d9e
Atsu Vovor <atsu.vovor@gmail.com>
2025-11-08 23:01:45 -0500

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -4,7 +4,8 @@ import os
 from typing import Optional, Dict, Any, Tuple
 import pandas as pd
 import numpy as np
-from config import RISK_COLOR_MAP, RISK_SCORE_MAP
+from config.config import LLM_MODEL_PATH, USE_RAG, RISK_COLOR_MAP
+
 
 # Constants
 JOIN_COLUMN = "ent_num"
