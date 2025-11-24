3e2a3be643d024342d917ad006b13cd915452f04
Atsu Vovor <atsu.vovor@gmail.com>
2025-11-24 01:58:46 -0500

    Update pivot_risk_visuals.py

--- pivot_risk_visuals.py
+++ pivot_risk_visuals.py
@@ -193,7 +193,15 @@ def generate_program_bar_chart(pivot_df: pd.DataFrame, selected_country=None, se
         color="Sanctions Program",
         title=f"SDN Concentration by Sanctions Program and Country{subtitle}",
     )
-    fig.update_layout(xaxis_tickangle=-45, yaxis_title="Total SDNs", height=900, legend_title="Sanctions Program", transition_duration=500)
+    #fig.update_layout(xaxis_tickangle=-45, yaxis_title="Total SDNs", height=900, legend_title="Sanctions Program", transition_duration=500)
+    # Enable stacked bar mode
+    fig.update_layout(
+        barmode="stack",
+        xaxis_tickangle=-45,
+        yaxis_title=y_col,
+        height=600,
+        margin=dict(t=70)
+    )
     return fig
 
 def generate_risk_donut_chart(filtered_df: pd.DataFrame, selected_country=None, selected_program=None) -> go.Figure:
